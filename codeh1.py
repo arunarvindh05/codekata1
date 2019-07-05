ea0fe60bdc215433286ed61edaa36effaa2de937
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-05 18:19:36 +0530

    Update codeh1.py

--- codeh1.py
+++ codeh1.py
@@ -1,3 +1,4 @@
+flag=0
 n=int(input())
 lst=list(map(int,input().split()))[:n]
 asd=[]
@@ -5,7 +6,10 @@ for i in range(0,n):
   if(lst.count(lst[i])>1):
     c=lst[i]
     asd.append(c)
+    flag=1
   sorted(asd)
   
 asd = list(dict.fromkeys(asd))
 print(*asd)
+if(flag==0):
+  print("unique")
