1e1651a568697c339a3879c3951eb6111e44567d
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-05 18:59:19 +0530

    Update codeh3.py

--- codeh3.py
+++ codeh3.py
@@ -1,11 +1,13 @@
-
+flag=0
 n=int(input())
 asd=[]
 lst=list(map(int,input().split()))
 for i in range(0,n):
   if(lst[i]==i):
     ele=lst[i]
+    flag=1
     asd.append(ele)
   asd=sorted(asd)
 print(*asd)
-  
+if(flag==0):
+  print("-1")  
