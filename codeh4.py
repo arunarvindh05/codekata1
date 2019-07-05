5e43fd37bcc9382b7f82bdd5ac3bea6951222d75
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-05 19:06:56 +0530

    Create codeh4.py

---
+++ codeh4.py
@@ -0,0 +1,12 @@
+n=int(input())
+lst=list(map(int,input().split()))[:n]
+asd=[]
+for i in range(0,n):
+  if(lst.count(lst[i])==1):
+    c=lst[i]
+    asd.append(c)
+    
+  sorted(asd)
+  
+asd = list(dict.fromkeys(asd))
+print(*asd)
