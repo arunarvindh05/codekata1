164572016bb749fef83a6e868e062515ddb33a51
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-05 19:32:44 +0530

    Update codepr2.py

--- codepr2.py
+++ codepr2.py
@@ -1,7 +1,12 @@
-asd=input()
-qwe=input()
-
-for i in range(0,len(asd)):
-  if(asd[0:i]==qwe[0:i]):
-    str1=asd[0:i]
-print(str1)    
+a=int(input())
+asd=[]
+for i in range(0,a):
+  ele=input()
+  asd.append(ele)
+if not asd: print('')
+s1 = min(asd)
+s2 = max(asd)
+for i, c in enumerate(s1):
+  if c != s2[i]:
+    print(s1[:i])
+print(s1)
