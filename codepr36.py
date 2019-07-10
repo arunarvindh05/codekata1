cd03dfb0d6c3aca9cdbac8b8ccb32826bcbe0163
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-10 19:20:30 +0530

    Create codepr36.py

---
+++ codepr36.py
@@ -0,0 +1,10 @@
+asd = int(input())
+qwe = [ int(x) for x in input().split()]
+asd = len(q)
+u = 0
+for i in range(0,asd-2):
+    for j in range(i+1, asd-1):
+        for k in range(j+1, asd):
+            if qwe[i] > qwe[j] > qwe[k] :
+                u =u+ 1
+print(u)
