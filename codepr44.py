3a34756018d32014f8d009449aa3e79bbf462fe6
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-12 11:50:39 +0530

    Create codepr44.py

---
+++ codepr44.py
@@ -0,0 +1,8 @@
+n,b,c,d=map(int,input().split())
+t=list(map(int,input().split()))
+z=[]
+for i in range(0,len(t)):
+  for j in range(i,len(t)):
+    for k in range(j,len(t)):
+      z.append(b*t[i]+c*t[j]+d*t[k])
+print(max(z))
