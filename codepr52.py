cf8162e7ad1ff8f190909642462d5667e27a60c9
arunarvindh05 <arunarvindh05@gmail.com>
2019-07-12 11:41:40 +0530

    Create codepr52.py

---
+++ codepr52.py
@@ -0,0 +1,13 @@
+import math
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+x4,y4=map(int,input().split())
+d1=math.sqrt(abs(((x1-x2)**2)+((y1-y2)**2)))
+d2=math.sqrt(abs(((x3-x4)**2)+((y3-y4)**2)))
+d3=math.sqrt(abs(((x2-x3)**2)+((y2-y3)**2)))
+d4=math.sqrt(abs(((x4-x1)**2)+((y4-y1)**2)))
+if(d1==d2==d3==d4):
+  print("yes")
+else:
+  print("no")  
